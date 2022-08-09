fbdff3f6a48805bb816cd8a0da7eb6adb4546ba7
jdw365 <99421864+jdw365@users.noreply.github.com>
2022-08-09 15:48:55 -0400

    finished file upload

--- HaskellIntro.hs
+++ HaskellIntro.hs
@@ -4,52 +4,70 @@ module HaskellIntro where
 
 import Set
 
--- Load this file into GHCi (say, with `ghci HaskellIntro.hs`) and type
--- `isThisWorking` at the prompt. GHCi will tell you whether it's working!
-
-isThisWorking :: String
-isThisWorking = "Yes"
-
 --
--- Problem 1
+--Q1
 --
 
+--1.1
 lastDigit :: Integer -> Integer
-lastDigit = error "lastDigit not yet defined"
-
+lastDigit n = n `mod` 10
 dropLastDigit :: Integer -> Integer
-dropLastDigit = error "dropLastDigit not yet defined"
+dropLastDigit n = n `div` 10
 
+--1.2
 toDigits :: Integer -> [Integer]
-toDigits = error "toDigits not yet defined"
+toDigits n | (n <= 0)  = reverse []
+           | otherwise = (toDigits (n `div` 10)) ++ [(n `mod` 10)]
 
+--1.3
+everyOtherTrue :: Int -> [Bool]
+everyOtherTrue n | (n == 0) = []
+                 | ((n `mod` 2) == 0) = True : everyOtherTrue (n - 1)
+                 | ((n `mod` 2) == 1) = False : everyOtherTrue (n - 1)
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther = error "doubleEveryOther not yet defined"
+doubleEveryOther [] = []
+doubleEveryOther (xs) | (head (everyOtherTrue (length xs))) = (2 * (head xs)) : doubleEveryOther (tail xs)
+                      | otherwise                           = (head xs) : doubleEveryOther (tail xs)
 
+--1.4
+sumDigitsHelper :: Integer -> Integer
+sumDigitsHelper n | (n == 0)  = 0
+                  | otherwise = (n `mod` 10) + sumDigitsHelper (n `div` 10)
 sumDigits :: [Integer] -> Integer
-sumDigits = error "sumDigits not yet defined"
+sumDigits [] = 0
+sumDigits (xs) = (sumDigitsHelper (head xs)) + sumDigits (tail xs)
 
+--1.5
 validate :: Integer -> Bool
-validate = error "validate not yet defined"
+validate n | (((sumDigits (doubleEveryOther (toDigits n))) `mod` 10) == 0) = True
+           | otherwise                                                     = False
 
 --
--- Problem 2
+--Q2
 --
 
+--2.1
+square :: Integer -> Integer
+square x = x * x
 pow :: (a -> a) -> Int -> a -> a
-pow = error "pow not yet defined"
+pow f 0 = id
+pow f n = f . (pow f (n - 1))
 
+--2.2
 g :: Integer -> Integer
-g = error "g not yet defined"
-
+g 0 = 0
+g n = n - (pow g 2 (n-1))
 h :: Integer -> Integer
-h = error "h not yet defined"
+h 0 = 0
+h n = n - (pow h 3 (n-1))
 
+--2.3
 d :: Int -> Integer -> Integer
-d = error "d not yet defined"
+d i 0 = 0
+d i n = n - (pow (d i) i (n-1))
 
---
--- Problem 3
---
 
-powerSet = error "powerSet not yet defined"
+--Q3
+powerSet :: Ord a => Set a -> Set (Set a)
+powerSet s | isEmpty s = singleton empty
+           | otherwise = union (mapSet (insert x) (powerSet xs)) (powerSet xs) where (x, xs) = split s
